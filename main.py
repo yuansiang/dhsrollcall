5c666bfb32441d32b2469dd1450ed7270b208ce8
Ng Yuan Siang <ng.yuansiang@dhs.sg>
2013-03-12 13:50:33 +0800

    rollcall_draft

--- main.py
+++ main.py
@@ -15,49 +15,89 @@
 # limitations under the License.
 #
 import webapp2
-import urllib
+import cgi
+  
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+	  self.response.out.write('''
+		<!doctype html>
+	<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>RollCall@DHS</title>
+		 <link rel="stylesheet" href="/static/themes/css/vanilla.css">
+        <script src="/static/src/lib/zepto.min.js" type="text/javascript" charset="utf-8"></script>
+        <script src="/static/src/jqtouch.min.js" type="text/javascript" charset="utf-8"></script>
+        
 
-import logging, os
+        <script src="/static/extensions/jqt.autotitles.min.js" type="application/x-javascript" charset="utf-8"></script>
 
-# Google App Engine imports.
-from google.appengine.ext.webapp import util
+        <script type="text/javascript" charset="utf-8">
+            var jQT = new $.jQTouch({
+                icon: 'jqtouch.png',
+                addGlossToIcon: false,
+                startupScreen: 'jqt_startup.png',
+                statusBar: 'black'
+            });
+        </script>
+    </head>
+    <body>
+        <div id="jqt">
+            <div id="page1">
+                <div class="toolbar">
+                    <h1>Attendance Taking</h1>
+                </div>
+                <ul class="edgetoedge">
+                    <li><a href="#attendance">Attendance Taking</a></li>
+                    <li><a href="#leaveform">Leave Form</a></li>
+                    <li><a href="#mc">MC Submission</a></li>
+                </ul>
+            </div>
+            <div id="attendance">
+                <div class="toolbar">
+                    <a href="#" class="back">Back</a>
+                    <h1><!-- Will be filled in --></h1>
+                </div>
+                <div class="info">
+                    The title for this page was automatically set from it&#8217;s referring link, no extra scripts required. Just include the extension and this happens.
+                </div>
+            </div>
+			<div id="leaveform">
+                <div class="toolbar">
+                    <a href="#" class="back">Back</a>
+                    <h1><!-- Will be filled in --></h1>
+                </div>
+                <div class="info">
+                    The title for this page was automatically set from it&#8217;s referring link, no extra scripts required. Just include the extension and this happens.
+                </div>
+            </div>
+			<div id="mc">
+                <div class="toolbar">
+                    <a href="#" class="back">Back</a>
+                    <h1><!-- Will be filled in --></h1>
+                </div>
+                <div class="info">
+                     <form method="post" action="%=blobstoreService.createUploadUrl("/upload")%" enctype="multipart/form-data"> 
+						 Description:<br> 
+						 <input type="text" name="form_description" size="40"> 
+						 <input type="hidden" name="MAX_FILE_SIZE" value="1000000"> 
+						 <br>File to upload:<br> 
+						 <input type="file" name="form_data" size="40"> 
+						 <p><input type="submit" name="submit" value="submit"> 
+					 </form> 
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+	  ''')
 
-# Force Django to reload its settings.
-from django.conf import settings
-settings._target = None
+class About(webapp2.RequestHandler):	  
+	def get(self):
+	  self.response.out.write('''
+	  HGaha
+	  ''')
 
-# Must set this env var before importing any part of Django
-# 'project' is the name of the project created with django-admin.py
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
-import logging
-import django.core.handlers.wsgi
-import django.core.signals
-import django.db
-import django.dispatch.dispatcher
-
-def log_exception(*args, **kwds):
-    logging.exception('Exception in request:')
-
-# Log errors.
-django.dispatch.dispatcher.connect(
-    log_exception, django.core.signals.got_request_exception)
-
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
-
-def main():
-    # Create a Django application for WSGI.
-    application = django.core.handlers.wsgi.WSGIHandler()
-
-    # Run the WSGI CGI handler with that application.
-    util.run_wsgi_app(application)
-
-if __name__ == '__main__':
-    main()		
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage), ('/about', About)],
+                              debug=True)
